6987a0ff5a3257bef7e3614144ee46da89b962ee
Chandanpreet91 <kaurchandanpreet91@gmail.com>
2020-12-04 13:38:16 -0800

    add review model and assoiciate with idea

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -2,6 +2,7 @@ class Idea < ApplicationRecord
 
 
     belongs_to :user 
+    has_many :reviews ,dependent: :destroy 
 
     validates :title, presence: true ,uniqueness: true 
     validates :description, presence: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_04_205101) do
+ActiveRecord::Schema.define(version: 2020_12_04_212435) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,14 @@ ActiveRecord::Schema.define(version: 2020_12_04_205101) do
     t.index ["user_id"], name: "index_ideas_on_user_id"
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.text "body"
+    t.bigint "idea_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["idea_id"], name: "index_reviews_on_idea_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
@@ -35,4 +43,5 @@ ActiveRecord::Schema.define(version: 2020_12_04_205101) do
   end
 
   add_foreign_key "ideas", "users"
+  add_foreign_key "reviews", "ideas"
 end
