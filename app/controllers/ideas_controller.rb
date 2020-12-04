ed49c301905c8829d734580215427645e9bcfd9b
Chandanpreet91 <kaurchandanpreet91@gmail.com>
2020-12-04 10:44:07 -0800

    added template for new action

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,27 +1,53 @@
 class IdeasController < ApplicationController
-
-        def new
+    before_action :find_idea, only: [:show,:edit,:update,:destroy]
+   # before_action :authenticate_user!, except: [:index, :show]
+    
+    def index
+       @ideas = Idea.all 
+    end
+    
+    def new
         @idea = Idea.new 
-        end
+     end
      
-        def create
-          @idea = Idea.new idea_params
-          if @idea.save
+     def create
+        @idea = Idea.new idea_params
+        if @idea.save
             flash[:notice] = "Idea created successfully"
             redirect_to idea_path(@idea)
-          else
+        else
             render :new 
-          end
         end
-    
+     end
+     def show 
+
+     end
      
+     def edit
     
+     end
+     
+     def update 
+        if @idea.update idea_params
+            redirect_to idea_path(@idea)
+        else
+            render :edit
+        end
+     end
+     
+     def destroy 
+        @idea.destroy
+        flash[:notice] = "Question destroyed"
+        redirect_to ideas_path 
+     end
 
 
  private
      def idea_params
        params.require(:idea).permit(:title, :description)
      end
-  
+     def find_idea
+        @idea = Idea.find params[:id]
+    end
    
 end
