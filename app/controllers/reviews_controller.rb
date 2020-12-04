cb86f6cd77e3531c02691857ce050673cf866a53
Chandanpreet91 <kaurchandanpreet91@gmail.com>
2020-12-04 15:28:39 -0800

    added authorization for reviews actions

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,26 +1,25 @@
 class ReviewsController < ApplicationController
-    def create
-        @idea = Idea.find params[:idea_id]
-        @review = Review.new review_params
-        @review.idea = @idea 
-        if @review.save 
-            redirect_to idea_path(@idea)
-        else
-            @review = @idea.reviews.order(created_at: :desc)
-            render 'ideas/show'
-        end
+    before_action :authenticate_user!
+  
+  def create
+    @idea = Idea.find params[:idea_id]
+    @review = Review.new params.require(:review).permit(:body)
+    @review.idea = @idea
+    @review.user= current_user
+    if @review.save
+      redirect_to idea_path(@idea)
+    else
+      render 'ideas/show'
     end
+  end
 
-    def destroy
-        @review = Review.find params[:id]
-        @review.destroy 
-        redirect_to idea_path(@review.idea)
+  def destroy
+    @review = Review.find params[:id]
+    if can?(:crud,@review)
+    @review.destroy
+    redirect_to idea_path(@review.idea)
+    else
+      head :unauthorized
     end
-
-
-
-    private
-    def review_params
-        params.require(:review).permit(:body)
-    end
+  end
 end
