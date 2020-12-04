04ce9853d162bac1428a7c187530ecec01fe5d3b
Chandanpreet91 <kaurchandanpreet91@gmail.com>
2020-12-04 10:34:01 -0800

    add tests for create action

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,10 +1,27 @@
 class IdeasController < ApplicationController
 
-    def new 
+        def new
         @idea = Idea.new 
-    end
+        end
+     
+        def create
+          @idea = Idea.new idea_params
+          if @idea.save
+            flash[:notice] = "Idea created successfully"
+            redirect_to idea_path(@idea)
+          else
+            render :new 
+          end
+        end
+    
+     
+    
 
 
-
-
+ private
+     def idea_params
+       params.require(:idea).permit(:title, :description)
+     end
+  
+   
 end
--- spec/controllers/ideas_controller_spec.rb
+++ spec/controllers/ideas_controller_spec.rb
@@ -17,7 +17,44 @@ RSpec.describe IdeasController, type: :controller do
         
         end
     end
-
+    describe "#create" do 
+        context "with valid parameters" do 
+            def valid_request
+                post(:create,params: {idea:FactoryBot.attributes_for(:idea)}) 
+            end
+            it "creates a idea in the db" do 
+                count_before = Idea.count 
+                valid_request
+                count_after = Idea.count
+                expect(count_after).to(eq(count_before + 1))
+            end
+            it "redirects us to show page for that idea" do 
+                valid_request
+                idea = Idea.last 
+                expect(response).to(redirect_to(idea_url(idea.id)))
+            end
+        end
+        context "with invalid parameters" do 
+            def invalid_request 
+                post(:create,params: {idea:FactoryBot.attributes_for(:idea,title: nil)})
+            end
+            it "doesn't save a idea in the db" do 
+                count_before = Idea.count 
+                invalid_request
+                count_after = Idea.count 
+                expect(count_after).to eq(count_before)
+            end 
+            it "renders the new template" do 
+                invalid_request
+                expect(response).to render_template(:new)
+            end
+            it "assigns an invalid job_post as an instance variable" do 
+                invalid_request
+                expect(assigns(:idea)).to be_a(Idea)
+                expect(assigns(:idea).valid?).to be(false)
+            end
+        end
+    end
 
 
 end
