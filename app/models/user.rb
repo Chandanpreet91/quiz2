c71eb069df75aad457a09efcb39dd00cc260733d
Chandanpreet91 <kaurchandanpreet91@gmail.com>
2020-12-04 16:30:16 -0800

    add like model and associated it

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
 
     has_many :ideas, dependent: :destroy 
     has_many :reviews, dependent: :destroy
+    has_many :likes 
+    has_many :liked_ideas, through: :likes, source: :idea 
 
     VALID_EMAIL_REGEX = /\A([\w+\-],?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
     validates :email, presence: true, uniqueness: true, format: VALID_EMAIL_REGEX
